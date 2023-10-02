6de0eb41278088094753f23bab7f613f6ff125b4
Raphaela Mendonça <raphaelalsmendonca@gmail.com>
2023-10-02 19:08:10 -0300

    Popula a tabela Produtos  utilizando o Migrations

---
+++ Migrations/20231002215936_PopulaProdutos.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace APICatalogo.Migrations
+{
+    /// <inheritdoc />
+    public partial class PopulaProdutos : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+             migrationBuilder.Sql("INSERT INTO Produtos(Nome, Descricao, Preco, ImagemUrl, Estoque, DataCadastro, CategoriaId) Values ('Coca-Cola Diet', 'Refrigerante de Cola 350ml', 5.45, 'coca-cola_diet.jpg', 50, now(), 1)");
+             migrationBuilder.Sql("INSERT INTO Produtos(Nome, Descricao, Preco, ImagemUrl, Estoque, DataCadastro, CategoriaId) Values ('Lanche de Atum', 'Lanche de atum com maionese', 8.50,'lanche-de-atum.jpg', 10, now(), 2)");
+             migrationBuilder.Sql("INSERT INTO Produtos(Nome, Descricao, Preco, ImagemUrl, Estoque, DataCadastro, CategoriaId) Values ('Pudim 100 g', 'Pudim de leite condensado 100g', 6.75,'pudim.jpg', 20, now(), 3)");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DELETE FROM Produtos");
+        }
+    }
+}
