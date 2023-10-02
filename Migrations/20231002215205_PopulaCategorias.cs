2a29249e77f2ec62556e4d7dd62665b49075a766
Raphaela Mendonça <raphaelalsmendonca@gmail.com>
2023-10-02 18:58:58 -0300

    Popula a tabela Categorias utilizando o Migrations

---
+++ Migrations/20231002215205_PopulaCategorias.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace APICatalogo.Migrations
+{
+    /// <inheritdoc />
+    public partial class PopulaCategorias : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("INSERT INTO Categorias(Nome, ImagemUrl) Values ('Bebidas', 'bebidas.jpg')");
+            migrationBuilder.Sql("INSERT INTO Categorias(Nome, ImagemUrl) Values ('Lanches', 'lanches.jpg')");
+            migrationBuilder.Sql("INSERT INTO Categorias(Nome, ImagemUrl) Values ('Sobremesas', 'sobremesas.jpg')");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DELETE FROM Categorias");
+        }
+    }
+}
