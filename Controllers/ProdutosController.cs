4e48a8f8e9eea2265116dfc7021ea0a39f05e931
Raphaela Mendonça <raphaelalsmendonca@gmail.com>
2023-10-02 19:43:57 -0300

    Cria o método Get(), responsável por retornar todos os produtos, em ProdutosController

--- Controllers/ProdutosController.cs
+++ Controllers/ProdutosController.cs
@@ -1,4 +1,5 @@
 using APICatalago.Context;
+using APICatalago.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace APICatalago.Controllers
@@ -15,6 +16,20 @@ namespace APICatalago.Controllers
             _context = context;
         }
 
-        
+        [HttpGet]
+        //IEnumerable<Produto> indica que se espera de retorno uma coleção de objetos do tipo Produto, pode-se também utilizar List<Produto> que significa a mesma coisa, porém é menos otimizado
+        //O recurso ActionResult permite que o método retorne tanto a lista de produtos, quanto todos os métodos pertencentes ao tipo de retorno suportados por ActionResult, como, por exemplo o NotFound()
+        public ActionResult<IEnumerable<Produto>> Get()
+        {
+            var produtos = _context.Produtos.ToList();
+
+            if(produtos is null)
+            {
+                return NotFound("Produtos não encontrados");
+            }
+            
+            return produtos;
+        }
+
     }
 }
