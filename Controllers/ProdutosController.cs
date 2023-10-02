bb452a3b49d0d22b5d15a61bb9fb90b650c2bb76
Raphaela Mendonça <raphaelalsmendonca@gmail.com>
2023-10-02 20:15:22 -0300

    Cria o método Delete(), responsável por excluir um produto, em ProdutosController

--- Controllers/ProdutosController.cs
+++ Controllers/ProdutosController.cs
@@ -75,5 +75,20 @@ namespace APICatalago.Controllers
             return Ok(produto);  
         }
 
+        [HttpDelete("{id:int}")]
+        public ActionResult Delete(int id)
+        {
+            var produto = _context.Produtos.FirstOrDefault(p => p.ProdutoId == id);
+
+            if(produto is null)
+            {
+                return NotFound("Produto não encontrado");
+            }
+
+            _context.Produtos.Remove(produto);
+            _context.SaveChanges();
+
+            return Ok(produto);
+        }
     }
 }
