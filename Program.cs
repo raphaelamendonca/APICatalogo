bf973c8418e6394cd39eaa22e2282b2401fb4b3e
Raphaela Mendonça <raphaelalsmendonca@gmail.com>
2023-10-04 19:19:26 -0300

    Adequações e melhorias relacionadas a serialização JSON

--- Program.cs
+++ Program.cs
@@ -9,8 +9,9 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers()
     .AddJsonOptions(options =>
     {
-        options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.Preserve;
+        options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;
     });
+    
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
