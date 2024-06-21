2692e10d854ade38ffbd69b6c12bd9294a472867
Raphaela Mendonça <raphaelalsmendonca@gmail.com>
2024-06-21 14:34:38 -0300

    Implementa os métodos Get(), GetById(), Post(), Put() e Delete() no controlador de Categorias

---
+++ APICatalogo/Controllers/CategoriasController.cs
@@ -0,0 +1,79 @@
+﻿using APICatalogo.Context;
+using APICatalogo.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace APICatalogo.Controllers;
+
+
+[Route("categorias")]
+[ApiController]
+
+public class CategoriasController : ControllerBase
+{
+    private readonly APICatalogoContext _context;
+
+    public CategoriasController(APICatalogoContext context)
+    {
+        _context = context;
+    }
+
+    [HttpGet]
+    public ActionResult<IEnumerable<Categoria>> Get()
+    {
+        var categorias = _context.Categorias.ToList();
+
+        if (!categorias.Any())
+            return NotFound("Categorias não encontradas");
+
+        return categorias;
+    }
+
+    [HttpGet("{id:int}", Name = "ObterCategoria")]
+    public ActionResult<Categoria> GetById(int id)
+    {
+        var categoria = _context.Categorias.FirstOrDefault(x => x.CategoriaId == id);
+
+        if (categoria is null)
+            return NotFound("Categoria não encontrada");
+
+        return categoria;
+    }
+
+    [HttpPost]
+    public ActionResult<Categoria> Post(Categoria categoria)
+    {
+        if (categoria is null)
+            return BadRequest();
+
+        _context.Categorias.Add(categoria);
+        _context.SaveChanges();
+
+        return new CreatedAtRouteResult("ObterCategoria", new { id = categoria.CategoriaId }, categoria);
+    }
+
+    [HttpPut("{id:int}")]
+    public ActionResult<Categoria> Put(int id, Categoria categoria)
+    {
+        if (id != categoria.CategoriaId)
+            return BadRequest();
+
+        _context.Entry(categoria).State = Microsoft.EntityFrameworkCore.EntityState.Modified;
+        _context.SaveChanges();
+
+        return Ok(categoria);
+    }
+
+    [HttpDelete("{id:int}")]
+    public ActionResult Delete(int id)
+    {
+        var categoria = _context.Categorias.FirstOrDefault(x => x.CategoriaId == id);
+
+        if (categoria is null)
+            return NotFound("Produto não encontrado");
+
+        _context.Categorias.Remove(categoria);
+        _context.SaveChanges();
+
+        return Ok(categoria);
+    }
+}
